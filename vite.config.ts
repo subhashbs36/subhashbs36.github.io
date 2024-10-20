2f0664a27e0a93e645b9381354496bbf7d522ca8
Subhash B S <subhashbs36@gmail.com>
2024-10-20 03:03:09 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
   //   outDir: "./dist/",
   // },
   plugins: [tsconfigPaths(), react()],
-  // base: "/subhashbs36.github.io/",
+  base: "/subhashbs36.github.io/",
 });
